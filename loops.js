e85cc4cabbe04329b7ce0286dac89fbcbf9b0fd8
chaudhryhamidali <ch.hamidali@gmail.com>
2023-01-25 22:59:24 +0100

    huiswerk

--- loops.js
+++ loops.js
@@ -5,7 +5,10 @@
 // Hoera!
 // Hoera!
 // ==========================================
-
+const print = "Hoera";
+        for(let i = 0; i <3; i++){
+            console.log(print)
+        }
 
 
 // ==========================================
@@ -18,6 +21,12 @@
 // klaar!
  // ==========================================
 
+        for(let i = 0; i <5; i++){
+            if (i < 4){
+                console.log("loop...")
+            }
+            else console.log("klaar!..")
+        }
 
 
 // ==========================================
@@ -29,6 +38,15 @@
 // 3 is oneven
 // .... etc.
 // 15 is oneven
+
+
+        for(let i = 0; i <16; i++) {
+            if( i % 2){
+                console.log(i, "is Oneven")
+            }
+            else console.log(i, " is even")
+        }
+
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
@@ -50,4 +68,13 @@
 // >> 8
 // >> 9
 // ==========================================
+for (let i = 0; i < 10; i++){
+    if(i < 3){
+        console.log(i)
+    }
+    else if(i < 7){
+        console.log(">", i)
+    }
+    else console.log(">>", i)
 
+}
